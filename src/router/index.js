27177fd6e1c712b90487fef80db7fbda4e2405ce
Joy-Chang-2021 <joy50422@gmail.com>
2022-01-25 03:10:26 +0800

    SignIn.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
+import SignIn from '../views/SignIn.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path:'/singin',
+    name: 'sign-in',
+    component: SignIn
+  },
   {
     path: '*',
     name: 'Not-found',
