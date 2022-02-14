0e39e03e625241555e70eb9d0810605c05eb6126
Joy-Chang-2021 <joy50422@gmail.com>
2022-02-14 17:24:07 +0800

    prepare for setting API

---
+++ src/utils/helpers.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import Swal from 'sweetalert2'
+
+export const apiHelper = axios.create({
+  baseURL: 'http://localhost:3000/api',
+});
+
+export const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+})
