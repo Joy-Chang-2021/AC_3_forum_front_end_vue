92663896feb94a8405215aa2e2218d9d3dd01888
Joy-Chang-2021 <joy50422@gmail.com>
2022-02-19 12:57:23 +0800

    RestaurantDashboard.vue / Connect restaurants API

--- src/views/RestaurantDashboard.vue
+++ src/views/RestaurantDashboard.vue
@@ -21,85 +21,8 @@
 </template>
 
 <script>
-const dummyData = {
-  id: 1,
-  name: "Ed Zieme",
-  tel: "(535) 363-9171 x859",
-  address: "163 Schultz Courts",
-  opening_hours: "08:00",
-  description: "quo",
-  image:
-    "https://loremflickr.com/320/240/restaurant,food/?random=15.46344947596352",
-  viewCounts: 1,
-  createdAt: "2022-01-24T13:30:57.000Z",
-  updatedAt: "2022-01-26T17:42:50.000Z",
-  CategoryId: 5,
-  Category: {
-    id: 5,
-    name: "素食料理",
-    createdAt: "2022-01-24T13:30:57.000Z",
-    updatedAt: "2022-01-24T13:30:57.000Z",
-  },
-  Comments: [
-    {
-      id: 1,
-      text: "Sed harum fuga.",
-      UserId: 2,
-      RestaurantId: 1,
-      createdAt: "2022-01-24T13:30:57.000Z",
-      updatedAt: "2022-01-24T13:30:57.000Z",
-      User: {
-        id: 2,
-        name: "user1",
-        email: "user1@example.com",
-        password:
-          "$2a$10$uSn.ry5E9Ay6jPnkIwT3uuS85zWHB3QCgRiFlojzk.nP0AfAZhwjm",
-        isAdmin: false,
-        image: null,
-        createdAt: "2022-01-24T13:30:57.000Z",
-        updatedAt: "2022-01-24T13:30:57.000Z",
-      },
-    },
-    {
-      id: 51,
-      text: "Reprehenderit et illo nobis sint odio voluptas expedita ut odit.",
-      UserId: 3,
-      RestaurantId: 1,
-      createdAt: "2022-01-24T13:30:57.000Z",
-      updatedAt: "2022-01-24T13:30:57.000Z",
-      User: {
-        id: 3,
-        name: "user2",
-        email: "user2@example.com",
-        password:
-          "$2a$10$8FO.bxFhdCUCud8/dLgyw.FUAYcJrjrgL0fq5EClUpOzxcB5zzZWG",
-        isAdmin: false,
-        image: null,
-        createdAt: "2022-01-24T13:30:57.000Z",
-        updatedAt: "2022-01-24T13:30:57.000Z",
-      },
-    },
-    {
-      id: 101,
-      text: "Repudiandae ut odit fugit est.",
-      UserId: 3,
-      RestaurantId: 1,
-      createdAt: "2022-01-24T13:30:57.000Z",
-      updatedAt: "2022-01-24T13:30:57.000Z",
-      User: {
-        id: 3,
-        name: "user2",
-        email: "user2@example.com",
-        password:
-          "$2a$10$8FO.bxFhdCUCud8/dLgyw.FUAYcJrjrgL0fq5EClUpOzxcB5zzZWG",
-        isAdmin: false,
-        image: null,
-        createdAt: "2022-01-24T13:30:57.000Z",
-        updatedAt: "2022-01-24T13:30:57.000Z",
-      },
-    },
-  ],
-};
+import restaurantsAPI from '../apis/restaurants'
+import { Toast } from '../utils/helpers'
 
 export default {
   data() {
@@ -108,17 +31,33 @@ export default {
     }
   },
   methods: {
-    fetchRestaurant() {
-      this.restaurant = {
-        name: dummyData.name,
-        categoryName: dummyData.Category ? dummyData.Category.name : '未分類',
-        commentsLength: dummyData.Comments.length,
-        viewCounts: dummyData.viewCounts,
-      }
+    async fetchRestaurant(restaurantId) {
+      try {
+        const {data, statusText} = await restaurantsAPI.getRestaurant({restaurantId})
+        if (statusText !== "OK") throw new Error(statusText)
+        const { restaurant } = data
+        this.restaurant = {
+          name: restaurant.name,
+          categoryName: restaurant.Category ? restaurant.Category.name : '未分類',
+          commentsLength: restaurant.Comments.length,
+          viewCounts: restaurant.viewCounts,
+        }
+      } catch {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
+      }  
     }
   },
   created() {
-    this.fetchRestaurant()
+    const { id } = this.$route.params
+    this.fetchRestaurant(id)
+  },
+  beforeRouteUpdate(to, from, next) {
+    const { id } = to.params
+    this.fetchRestaurant(id)
+    next()
   }
 };
 </script>
